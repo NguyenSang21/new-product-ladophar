7cfc8e08943700f576819c992c8dbb936d25180f
nguyen sang <nguyentansang1996@gmail.com>
2022-06-23 20:28:32 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import MyModal from '../components/MyModal'
 import Confetti from 'react-confetti'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import PlayAudio from '../components/PlayAudio'
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 const BG_AUDIO = 'nhac-nen.mp3';
 
@@ -19,8 +20,24 @@ const Home: NextPage = () => {
     setOpenConfetti(true);
     setTimeout(() => {
       setOpenConfetti(false);
+      handleAutoScroll();
     }, 10000);
     setAudioUrl(BG_AUDIO);
+  }
+
+  const handleAutoScroll = () => {
+    let scrollID = 1;
+    const i = setInterval(() => {
+      if(scrollID > 5) {
+        clearInterval(i);
+      }
+      scroller.scrollTo(`scroll-to-${scrollID}`, {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+      })
+      scrollID += 1
+    }, 5000);
   }
 
   return (
@@ -42,51 +59,61 @@ const Home: NextPage = () => {
           height={1196}
         />
       </div>
-      <div className="flex content-center items-center py-10">
-        <div className="flex w-1/2 justify-center">
-          <Image
-            alt='Mountains'
-            src='/ladophar_phone.gif'
-            width={1080}
-            height={932}
-          />
-        </div>
-        <div className="flex w-1/2 justify-center">
-          <div className="text-center text-5xl text-[#5F4C8B] font-semibold space-y-2">
-            <p>Gian hàng và sản phẩm Ladophar</p>
-            <p>trên ứng dụng ECO Merchant</p>
+      <Element name="scroll-to-1">
+        <div className="flex content-center items-center py-10">
+          <div className="flex w-1/2 justify-center">
+            <Image
+              alt='Mountains'
+              src='/ladophar_phone.gif'
+              width={1080}
+              height={932}
+            />
+          </div>
+          <div className="flex w-1/2 justify-center">
+            <div className="text-center text-5xl text-[#5F4C8B] font-semibold space-y-2">
+              <p>Gian hàng và sản phẩm Ladophar</p>
+              <p>trên ứng dụng ECO Merchant</p>
+            </div>
           </div>
         </div>
-      </div>
-      <Image
-        alt='Mountains'
-        src='/LADOPHAR_LP-04.png'
-        layout='responsive'
-        quality={100}
-        width={1920} 
-        height={806}
-      />
-      <Image
-        alt='Mountains'
-        src='/banner2.png'
-        quality={100}
-        width={1920} 
-        height={889}
-      />
-      <Image
+      </Element>
+      <Element name="scroll-to-2">
+        <Image
+          alt='Mountains'
+          src='/LADOPHAR_LP-04.png'
+          layout='responsive'
+          quality={100}
+          width={1920} 
+          height={806}
+        />
+      </Element>
+      <Element name="scroll-to-3">
+        <Image
+          alt='Mountains'
+          src='/banner2.png'
+          quality={100}
+          width={1920} 
+          height={889}
+        />
+      </Element>
+      <Element name="scroll-to-4">
+        <Image
           alt='Mountains'
           src='/LADOPHAR_LP-05.png'
           quality={100}
           width={1920} 
           height={1394}
         />
-      <Image
-        alt='Mountains'
-        src='/LADOPHAR_LP-07.png'
-        quality={100}
-        width={1920} 
-        height={351}
-      />
+      </Element>
+      <Element name="scroll-to-5">
+        <Image
+          alt='Mountains'
+          src='/LADOPHAR_LP-07.png'
+          quality={100}
+          width={1920} 
+          height={351}
+        />
+      </Element>
       {
         isOpen ? <MyModal isOpen={isOpen} onClose={handleOnClose} /> : null
       }
